3aceefcd2c24835a2c592d9da03a857a153efedb
P.O Marec <po@marec.me>
2025-06-01 20:47:29 +0200

    bookmark folders

--- script/main.js
+++ script/main.js
@@ -1,36 +1,31 @@
-const browserAPI = typeof browser === 'undefined' ? chrome : browser;
-const subTree = typeof browser === 'undefined' ? '1' : 'toolbar_____';
+const generateBookmarkItem = (item) => {
+    switch (item.type) {
+        case 'folder':
+            return `</div><div class="item-group-title">${item.title}</div><div class="item-group">${generateBookmarkItems(item.children)}</div>`
+        case 'separator':
+            return '</div><div class="item-group">'
+        case 'bookmark':                
+            const faviconUrl = `https://www.google.com/s2/favicons?sz=32&domain_url=${encodeURIComponent(item.url)}`
+            return (
+                `<a class='item' href='${item.url}'>
+                    <div class='item__icon'>
+                        <img src='${faviconUrl}'/>
+                    </div>
+                    <div class='item__title'>
+                        ${item.title}
+                    </div>
+                    <div class='item__url'>
+                        ${item.url}
+                    </div>
+                </a>`
+            )
+    }
+}
 
-browserAPI.storage.sync.get('options', () =>
-    browserAPI.bookmarks.getSubTree(subTree, items =>
+const generateBookmarkItems = (items ) => items.map(generateBookmarkItem).join(' ')
+
+browser.storage.sync.get('options', () =>
+    browser.bookmarks.getSubTree('toolbar_____', items =>
         document.querySelector(".main").innerHTML = `<div class="item-group">${generateBookmarkItems(items[0].children)}</div>`
     )
-);
-
-
-const generateBookmarkItems = (item) => {
-    if (Array.isArray(item)) {
-        return item.map(obj => generateBookmarkItems(obj)).join('');
-    } else if (item.url === 'data:') {
-        return '</div><div class="item-group">'
-    } else if (item.url === 'about:blank') {
-        return ''
-    } else if (item.url !== undefined) {
-
-        const faviconUrl = `https://www.google.com/s2/favicons?sz=32&domain_url=${encodeURIComponent(item.url)}`;
-
-        return (
-            `<a class='item' href='${item.url}'>
-                <div class='item__icon'>
-                    <img src='${faviconUrl}'/>
-                </div>
-                <p class='item__title'>
-                    ${item.title}
-                </p>
-                <div class='item__url'>
-                    ${item.url}
-                </div>
-            </a>`
-        );
-    } else return ''
-}
+)
