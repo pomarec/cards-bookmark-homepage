dbdfc40bb65ad24d3e0f9ea7ecab4384aef37a87
P.O Marec <po@marec.me>
2025-06-01 20:24:10 +0200

    Improved layout

--- script/main.js
+++ script/main.js
@@ -3,33 +3,30 @@ const subTree = typeof browser === 'undefined' ? '1' : 'toolbar_____';
 
 browserAPI.storage.sync.get('options', () =>
     browserAPI.bookmarks.getSubTree(subTree, items =>
-        createBookmarks(items)
+        document.querySelector(".main").innerHTML = `<div class="item-group">${generateBookmarkItems(items[0].children)}</div>`
     )
 );
 
-const createBookmarks = (items) => {
-    document.querySelector(".main").innerHTML = `<div class="item-group">${generateBookmarkItem(items[0].children)}</div>`;
-};
 
-const generateBookmarkItem = (item) => {
+const generateBookmarkItems = (item) => {
     if (Array.isArray(item)) {
-        return item.map(obj => generateBookmarkItem(obj)).join('');
+        return item.map(obj => generateBookmarkItems(obj)).join('');
     } else if (item.url === 'data:') {
         return '</div><div class="item-group">'
     } else if (item.url === 'about:blank') {
         return ''
     } else if (item.url !== undefined) {
 
-        const faviconUrl = `https://www.google.com/s2/favicons?sz=16&domain_url=${encodeURIComponent(item.url)}`;
+        const faviconUrl = `https://www.google.com/s2/favicons?sz=32&domain_url=${encodeURIComponent(item.url)}`;
 
         return (
             `<a class='item' href='${item.url}'>
                 <div class='item__icon'>
                     <img src='${faviconUrl}'/>
                 </div>
-                <div class='item__title'>
+                <p class='item__title'>
                     ${item.title}
-                </div>
+                </p>
                 <div class='item__url'>
                     ${item.url}
                 </div>
